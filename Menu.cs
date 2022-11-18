f87866758de9be8996d886b3f18c9706178210b4
oriolli <mar.orioli@hotmail.com>
2022-11-18 10:59:31 -0300

    desenvolvendo o menu

--- Menu.cs
+++ Menu.cs
@@ -11,6 +11,10 @@ namespace EditorHtml
       Console.ForegroundColor = ConsoleColor.Black;
 
       DrawScreen();
+      WriteOptions();
+
+      var option = short.Parse(Console.ReadLine());
+      HandleMenuOprtion(option);
     }
     public static void DrawScreen()
     {
@@ -36,6 +40,39 @@ namespace EditorHtml
 
       Console.Write("+");
       Console.Write("\n");
+    }
+    public static void WriteOptions()
+    {
+      Console.SetCursorPosition(3, 2);
+      Console.WriteLine("Editor HTML");
+      Console.SetCursorPosition(3, 3);
+      Console.WriteLine("===========================");
+      Console.SetCursorPosition(3, 4);
+      Console.WriteLine("Selecione uma opção abaixo");
+      Console.SetCursorPosition(3, 6);
+      Console.WriteLine("1 - Novo arquivo");
+      Console.SetCursorPosition(3, 7);
+      Console.WriteLine("2 - Abrir");
+      Console.SetCursorPosition(3, 9);
+      Console.WriteLine("0 - Sair");
+      Console.SetCursorPosition(3, 10);
+      Console.WriteLine("Opção: ");
+
+    }
+    public static void HandleMenuOprtion(short option)
+    {
+      switch (option)
+      {
+        case 1: Console.WriteLine("Editor"); break;
+        case 2: Console.WriteLine("View"); break;
+        case 0:
+          {
+            Console.Clear();
+            Environment.Exit(0);
+            break;
+          }
+        default: Show(); break;
+      }
     }
   }
 }
